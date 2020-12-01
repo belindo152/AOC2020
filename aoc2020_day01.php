cd62b14f565f6415d65522b01ff85db5e692bd42
belindo152 <16253055+belindo152@users.noreply.github.com>
2020-12-01 09:17:34 +0100

    Update aoc2020_day01.php

--- aoc2020_day01.php
+++ aoc2020_day01.php
@@ -7,7 +7,7 @@
 	$target = 2020;
 
 	echo '<strong>Part 1:</strong><br/>';
-    for ($i = 0; $i < $count; $i++) {
+    	for ($i = 0; $i < $count; $i++) {
 		for ($j = $i + 1; $j < $count; $j++) {
 			if ($input[$i] + $input[$j] == $target) {
 				echo 'The two numbers adding up to 2020 are: '.$input[$i].' and '.$input[$j];
@@ -15,11 +15,11 @@
 				goto partTwo;
 			}
 		}
-    }
+   	}
 
 partTwo:
 	echo '<br /><br /><strong>Part 2:</strong><br/>';
-    for ($i = 0; $i < $count; $i++) {
+    	for ($i = 0; $i < $count; $i++) {
 		for ($j = $i + 1; $j < $count; $j++) {
 			for ($k = $j + 1; $k < $count; $k++) {
 				if ($input[$i] + $input[$j] +  $input[$k] == $target) {
@@ -29,7 +29,7 @@ partTwo:
 				}
 			}
 		}
-    }
+    	}
 
 theEnd:
 	exit();
